f48b3345156ec162e853489a22e3bb65a76dead5
LukaszKoziarski <lkoziars@gmail.com>
2017-03-21 00:19:06 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.listen(3000, function () {
+    console.log('Server listening on port: 3000!')
+});
+
+app.get('/', function (req, res) {
+    res.send('Homework API - Testing class - Lukasz Koziarski');
+});
+
+app.post('/interest', function(req, res) {
+    var interest = countInterest(req.body.amount);
+    res.status(200).send(String(interest));
+});
+
+app.post('/discount', function(req, res) {
+    var discount = countDiscount(req.body.newCust, req.body.loyalCard, req.body.coupon);
+    res.status(200).send(String(discount));
+});
+
+// 404
+app.use(function(req, res, next) {
+    res.send("Error 404: Not found!");
+});
+
+function countInterest(amount){
+    var interest = "";
+
+    if (amount >= 0.00 && amount <= 100.00) {
+        interest = amount * 0.03;
+    } else if (amount >= 100.01 && amount <= 999.99) {
+        interest = amount * 0.05;
+    } else if (amount >= 1000.00) {
+        interest = amount * 0.07;
+    }
+    return interest;
+}
+
+function countDiscount(newCust, loyal, coupon) {
+    var discount = "";
+
+    if ((newCust && !loyal && coupon) || (!newCust && !loyal && coupon)) {
+        discount = 20;
+    } else if (newCust && !loyal && !coupon) {
+        discount = 15;
+    } else if (!newCust && loyal && coupon) {
+        discount = 30;
+    } else if (!newCust && loyal && !coupon) {
+        discount = 10;
+    } else if (!newCust && !loyal && !coupon) {
+        discount = 0;
+    }
+
+    return discount;
+}
